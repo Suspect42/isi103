1941a2d0f3b9eb19dc564f3ceb80cb3bf74e10a9
Bruno <43757720+Suspect42@users.noreply.github.com>
2020-05-22 16:13:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT; // || 3000;
+const port = process.env.PORT;
 
 const verifyRoute = require('./routes');
 const jasminRequests = require('./jasminRequests');
