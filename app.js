a2217fc00a3e46fdb8aaaed37dee511055ce93e2
Bruno <43757720+Suspect42@users.noreply.github.com>
2020-06-03 16:14:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -48,7 +48,6 @@ app.post('/api/carrinho', function(req, res){
     console.log(req.body)
     carrinho.setArtigos(req.body, function(){
         res.send('Artigos adicionados ao carrinho!');
-        console.log(carrinho.artigos)
     });
 });
 
