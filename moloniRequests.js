ca04e617fc0121fb768de72e43f8c826f468c948
Bruno <43757720+Suspect42@users.noreply.github.com>
2020-06-04 15:26:33 +0100

    Update moloniRequests.js

--- moloniRequests.js
+++ moloniRequests.js
@@ -175,6 +175,7 @@ function postProduct(name, price, callback) {
     request(options, function (error, response) {
         if (error) throw new Error(error);
         body = JSON.parse(response.body);
+        console.log(body)
         return callback(body);
     });
 };
