1f2ceca421b7af20f6b8801916da993b9401c852
Bruno <43757720+Suspect42@users.noreply.github.com>
2020-06-08 19:51:15 +0100

    Create qrcode.js

---
+++ qrcode.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+var FA = '';
+
+function setFA(fa){
+    FA = fa;
+}
+
+function getQRCode(callback) {
+    var options = {
+        'method': 'GET',
+        'url': 'http://api.qrserver.com/v1/create-qr-code/?data=' + JSON.stringify(FA) + '&size=300x300',
+        'headers': {
+        }
+    };
+    request(options, function (error, response) {
+        if (error) throw new Error(error);
+        console.log(response.body);
+        return callback(response.body)
+    });
+}
+
+module.exports.setFA = setFA;
+module.exports.getQRCode = getQRCode;
